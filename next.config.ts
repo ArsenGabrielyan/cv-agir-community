39ae505df9ee0f3358a7eed70d1eb857dd255a7b
ArsenGabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2026-04-19 22:00:17 +0400

    added `unsafe-eval`

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
               key: "Content-Security-Policy",
               value: [
                 "default-src 'self'",
-                "script-src 'self' https://www.google.com https://www.gstatic.com 'unsafe-inline' ",
+                "script-src 'self' https://www.google.com https://www.gstatic.com 'unsafe-inline' 'unsafe-eval'",
                 "style-src 'self' 'unsafe-inline'",
                 "img-src * blob: data:",
                 "font-src 'self' data:",
